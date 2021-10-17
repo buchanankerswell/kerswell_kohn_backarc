07501299dda9008e4e3848f43ee61418a439ba7b
Buchanan Kerswell <buck.kerswell@gmail.com>
2021-10-17 02:20:39 -0600

    refactor preprocessing and variogram building/fits
    - new optimization and cost function
    - crop hf data and interpolations to buffer
    - use local kriging for better estimates

--- interp_diff.R
+++ interp_diff.R
@@ -1,102 +1,163 @@
 # Load functions and libraries
+cat(rep('~', 60), '\n', sep='')
+cat('Loading packages and functions ...\n\n')
 source('functions.R')
 load('data/hf.RData')
+cat(rep('~', 60), sep='')
 
-# Bounding Boxes
-cat('\nDefining bounding boxes')
-purrr::map(seg.names,
-  ~shp.sa.segs.robin.pacific.buffer %>%
-  filter(segment == .x) %>%
-  st_bbox() %>%
-  bbox_widen(crs = proj4.robin.pacific,
-  borders = c('top' = 0.1,
-              'bottom' = 0.1,
-              'left' = 0.1,
-              'right' = 0.1))) %>%
-  purrr::set_names(nm = seg.names) -> shp.box
+# Optimizing Kriging
+opt.fun <- function(x) {
+  cost.function(
+    shp.hf = shp.hf.crop[[3]],
+    cutoff.prop = x[1],
+    n.lags = x[2],
+    lag.start = x[3],
+    nmax = x[4],
+    model.vgrm = x[5],
+    verbose = T
+  )
+}
 
-# Crop data
-cat('\nCropping heat flow data')
-purrr::map(shp.box,
-  ~shp.hf %>%
-  rename(hf = `heat-flow (mW/m2)`) %>%
-  st_crop(.x)) %>%
-  purrr::set_names(nm = seg.names) -> shp.hf.crop
-
-# Cropped Grid
-cat('\nDefining interpolation grids')
-purrr::map(shp.box, ~shp.grid %>% st_crop(.x)) %>%
-  purrr::set_names(nm = seg.names) -> shp.grid.crop
-
-# Define variograms manually
-# Define variogram cutoffs for experimental variograms
-cat('\nDefining variogram cutoffs for experimental variograms')
-cutoff <- list(5, 6, 4, 5, 3, 8, 6, 3, 5, 15, 7, 3, 5) %>%
-  purrr::set_names(seg.names)
-lags <- list(15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15) %>%
-  purrr::set_names(seg.names)
-lag.start <- list(3, 1, 1, 2, 5, 1, 3, 1, 2, 1, 1, 1, 3)
+opt.solver <-
+  lbfgs(
+    x0 = c(3, 15, 1, 25, 0),
+    fn = opt.fun,
+    lower = c(1, 15, 1, 20, 0),
+    upper = c(10, 115, 5, 500, 3),
+    control = list(xtol_rel = 1e-6, maxeval = 2000)
+  )
 
 # Calculate experimental variogram
-cat('\nCalculating experimental variograms')
-purrr::pmap(list(
-  shp.hf.crop,
-  cutoff,
-  lags,
-  lag.start,
-  seg.names), ~{
-  cutoff <- max(st_distance(..1))/..2
-  lags <- ..3
-  width <- as.vector(cutoff)/lags
-  shift.cutoff <- width*(..3+..4)
-  v <- variogram(hf~1,
-  locations = .x,
-  cutoff = shift.cutoff,
-  width = width)
-  v[..4:nrow(v),]
-}) %>%
-purrr::set_names(nm = seg.names) -> v.grms
+cat('\n', rep('~', 60), sep='')
+cat('\nCalculating experimental variograms ...\n')
+
+experimental.vgrms <- map(shp.hf.crop, experimental_vgrm)
 
 # Arbitrary variogram models
 cat('\nDefining variogram models')
-purrr::map(1:13, ~vgm(model = c('Sph', 'Exp'))) %>%
-purrr::set_names(nm = seg.names)-> v.mods
+
+model.vgrms <- map(seq_along(experimental.vgrms), ~vgm(NA, 'Sph', NA, NA))
 
 # Fit experimental variograms
 cat('\nFitting variograms')
-purrr::map2(v.grms, v.mods, ~{fit.variogram(.x, model = .y)}) %>%
-purrr::set_names(seg.names) -> v.fits
 
-cat('\nPlotting variogram models and saving to figs/vgrms.png')
-purrr::pmap(list(v.grms, v.fits, seg.names), ~plot_vgrm(..1, ..2, ..3)) %>%
-wrap_plots() -> p
+fitted.vgrms <-
+  map2(experimental.vgrms, model.vgrms, ~{
+    suppressWarnings({
+      fit.variogram(.x, model = .y, fit.method = 7)})
+  })
 
 # Save plot
+cat('\nPlotting variogram models and saving to figs/vgrms.png')
+cat('\n', rep('~', 60), sep='')
+
+p <-
+  list(experimental.vgrms, fitted.vgrms, seg.names) %>%
+  pmap(plot_vgrm) %>%
+  wrap_plots()
+
 ggsave(
   'figs/vgrms.png',
   plot = p,
   device = 'png',
   type = 'cairo',
-  width = 7,
-  height = 7
+  width = 6,
+  height = 6
 )
 
-# Interpolation difference
-cat('\nCalculating interpolation differences\n')
-pb <- progress::progress_bar$new(
-  format = "Interpolating [:bar] :percent in :elapsed",
-  total = length(seg.names), clear = FALSE, width= 60)
-purrr::pwalk(list(
-  seg.names,
-  shp.hf.crop,
-  v.grms,
-  v.mods,
-  shp.grid.crop),
-  ~{pb$tick()
-    cat('\n', ..1, '\n')
-    Krige_diff(..1, ..2, ..3, ..4, ..5,
-               param = 'hf',
-               data.compare = shp.hf.pred,
-               path = 'data/diff/')})
+cat('\n', rep('~', 60), sep='')
+# Calculate refined experimental variogram
+cat('\n', rep('~', 60), sep='')
+cat('\nCalculating refined experimental variograms ...\n')
 
-cat('\nDone\n')
+# Define refined experimental variogram parameters
+cutoff.prop <- c(5, 6, 4, 5, 3, 8, 6, 3, 5, 15, 7, 3, 5)
+n.lags <- c(15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15)
+lag.start <- c(3, 1, 1, 2, 5, 1, 3, 1, 2, 1, 1, 1, 3)
+
+pwalk(list(seg.names, cutoff.prop, n.lags, lag.start), ~{
+  cat(
+    '\nSegment:         ', ..1,
+    '\nVariogram cutoff:', ..2, 
+    '\nNumber of lags:  ', ..3,
+    '\nLag shift:       ', ..4,
+    '\n'
+  )
+})
+
+experimental.vgrms <-
+  pmap(
+    list(
+      shp.hf.crop,
+      cutoff.prop,
+      n.lags,
+      lag.start
+    ),
+    ~experimental_vgrm(..1, ..2, ..3, ..4)
+  ) %>%
+  set_names(nm = seg.names)
+
+# Arbitrary variogram models
+cat('\nDefining variogram models')
+
+model.vgrms <-
+  map(
+    seq_len(length(experimental.vgrms)),
+    ~vgm(NA, 'Sph', NA, NA)
+  ) %>%
+  set_names(nm = seg.names)
+
+# Fit experimental variograms
+cat('\nFitting variograms')
+
+fitted.vgrms <-
+  map2(experimental.vgrms, model.vgrms, ~{
+    suppressWarnings(fit.variogram(.x, model = .y, fit.method = 7))
+  }) %>%
+  set_names(seg.names)
+
+# Save plot
+cat('\nPlotting variogram models and saving to figs/vgrms.png')
+cat('\n', rep('~', 60), sep='')
+
+p <- pmap(list(experimental.vgrms, fitted.vgrms, seg.names), plot_vgrm) %>% wrap_plots()
+
+ggsave(
+  'figs/refined_vgrms.png',
+  plot = p,
+  device = 'png',
+  type = 'cairo',
+  width = 6,
+  height = 6
+)
+
+cat('\n', rep('~', 60), sep='')
+# Kriging segments
+cat('\n', rep('~', 60), sep='')
+cat('\nKriging segments ...\n')
+
+shp.interp.krige <-
+  list(shp.hf.crop, fitted.vgrms, shp.grid.crop) %>%
+  pmap(~Krige(..1, ..2, ..3, nmax=200)) %>%
+  set_names(seg.names)
+
+# Calculating interpolation difference
+cat('\nCalculating interpolation differences')
+
+shp.interp.diff <-
+  shp.interp.krige %>%
+  map(~interp_diff(.x, shp.interp.luca)) %>%
+  set_names(seg.names)
+
+cat('\n', rep('~', 60), sep='')
+cat('\nDone!\n')
+cat('\n', rep('~', 60), sep='')
+
+p1 <- 
+  ggplot(data=shp.interp.diff[[5]]) +
+  geom_sf(aes(color=est.krige)) +
+  geom_sf(data=shp.buffer[[5]], color='white', fill=NA) +
+  geom_sf(data=shp.hf.crop[[5]], color='deeppink', size=0.5, shape=15)
+ggplot(data=shp.interp.diff[[5]])+geom_sf(aes(color=est.similarity)) -> p2
+ggplot(data=shp.interp.diff[[5]])+geom_sf(aes(color=est.diff)) -> p3
+p1 / p2 / p3
---
+++ preprocess.R
@@ -0,0 +1,296 @@
+# Load packages and functions
+cat(rep('~', 60), '\n', sep='')
+cat('Loading packages and functions ...\n\n')
+source('functions.R')
+cat(rep('~', 60), sep='')
+
+# Projections
+# WGS84
+proj4.wgs <-
+  '+proj=longlat +lon_wrap=180 +ellps=WGS84 +datum=WGS84 +no_defs'
+
+# Robinson Pacific centered
+proj4.RP <-
+  '+proj=robin +lon_0=-155 +lon_wrap=-155 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs'
+
+# Print Projections
+cat('\n', rep('~', 60), '\n', sep='')
+cat(
+  'Defining coordinate reference system ...\n',
+  '\nWGS:\n',
+  proj4.wgs,
+  '\nRobinson Pacific Centered:\n',
+  proj4.RP
+)
+cat('\n', rep('~', 60), '\n', sep='')
+
+# Country Boundaries
+
+# Create sliver around twenty-five degree long to
+# cut country boundaries so they don't project
+# as straight lines across the globe
+shp.sliver <-
+  st_polygon(
+    x = list(rbind(
+      c(25.000001, 90),
+      c(25, 90),
+      c(25, -90),
+      c(25.000001, -90),
+      c(25.000001, 90))
+    )
+  ) %>%
+  st_sfc() %>%
+  st_set_crs(proj4.wgs)
+
+# Trim the countries boundaries overlapping at zero degree long and reproject
+shp.countries <-
+  ne_countries(returnclass = 'sf')
+
+suppressWarnings({
+  suppressMessages({
+    shp.world <-
+      shp.countries %>%
+      st_difference(shp.sliver) %>%
+      as_tibble() %>%
+      st_as_sf() %>%
+      st_transform(proj4.RP)
+  })
+})
+
+# Read Syracuse et al (2006) volcanoes
+cat(rep('~', 60), '\n', sep='')
+cat('Reading Syracuse and Abers (2006) data ...\n')
+
+volc <-
+  read_table(
+    'data/sa2006/volcanoes.txt',
+    col_type = c('ddddddddddddccf'),
+    na = 'NULL'
+  )
+
+volc.no.spread <-
+  read_table(
+    'data/sa2006/volcanoes_nospread.txt',
+    col_type = c('ddddddddddddccf'),
+    na = 'NULL'
+  )
+
+# Change to simple features object and project
+shp.volc <-
+  volc %>%
+  st_as_sf(coords = c('Lon', 'Lat')) %>%
+  st_set_crs(proj4.wgs) %>%
+  st_transform(proj4.RP)
+
+shp.volc.no.spread <-
+  volc.no.spread %>%
+  rename(Vcorr = Vc) %>%
+  st_as_sf(coords = c('Lon', 'Lat')) %>%
+  st_set_crs(proj4.wgs) %>%
+  st_transform(proj4.RP)
+
+cat('\nFound', nrow(volc) + nrow(volc.no.spread) , 'volcanoes\n')
+
+# Read syracuse et al 2006 Segments
+files <-
+  list.files('data/sa2006/gmts', full.names = TRUE)
+
+seg.names <-
+  files %>%
+  str_extract('(?<=gmts\\/)[a-z].*(?=_contours\\.gmt)') %>%
+  str_replace_all('_', ' ') %>%
+  str_to_title()
+
+cat('\nFound segments:', seg.names, sep = '\n')
+cat(rep('~', 60), sep='')
+
+# Read contours and project to robinson pacific centered
+shp.contours <-
+  files %>%
+  map(~read_latlong(.x, proj4.RP)) %>%
+  set_names(seg.names)
+
+# First contour defines the segment boundaries
+shp.segs <-
+  shp.contours %>%
+  map(~.x[1,])
+
+# Read ThermoGlobe database from Lucazeau (2019)
+cat('\n', rep('~', 60), sep='')
+cat('\nReading ThermoGlobe data from Lucazeau (2019) ...\n')
+
+# Read ThermoGlobe data from Lucazeau (2019)
+hf <- read_csv('data/tglobe/tglobe.csv', col_type = cols())
+
+cat(
+  '\nRemoving', nrow(hf[is.na(hf$`heat-flow (mW/m2)`),]), 'NA heat flow values',
+  '\nRemoving', nrow(hf[hf$code6 == 'D',]), 'poor quality data (code6 = D)',
+  '\nRemoving', nrow(hf[is.na(hf$latitude) | is.na(hf$longitude),]), 'obs without coords\n'
+)
+
+# Transform to sf object
+shp.hf <-
+  hf %>%
+  filter(!is.na(longitude)) %>% # remove no lat
+  filter(!is.na(latitude)) %>% # remove no long
+  st_as_sf(coords = c(9,10), crs = proj4.wgs) %>% # make sf object
+  st_transform(proj4.RP)
+
+# Filter bad quality data and missing heat flow
+shp.hf.filtered <-
+  hf %>% 
+  filter(!is.na(`heat-flow (mW/m2)`)) %>% # remove NA
+  filter(code6 != 'D') %>% # remove poor quality data
+  filter(!is.na(longitude)) %>% # remove no lat
+  filter(!is.na(latitude)) %>% # remove no long
+  st_as_sf(coords = c(9,10), crs = proj4.wgs) %>% # make sf object
+  st_transform(proj4.RP)
+
+# Check for duplicate measurements and remove
+dup <- sp::zerodist(sf::as_Spatial(shp.hf.filtered))
+
+cat('\nParsing', length(dup), 'duplicate measurements:\n')
+cat('\n', rep('+', 40), sep='')
+cat(
+  '\nIf x is better quality than y, keep x',
+  '\notherwise randomly keep either x or y'
+)
+cat('\n', rep('+', 40), sep='')
+
+# For duplicate measurements, select the best quality data point,
+# if the quality is the same, randomly select one measurement
+rid <- vector('integer', nrow(dup))
+
+for(i in 1:nrow(dup)) {
+  if(
+    shp.hf.filtered$code6[dup[i,1]] != shp.hf.filtered$code6[dup[i,2]] &
+    shp.hf.filtered$code6[dup[i,1]] > shp.hf.filtered$code6[dup[i,2]]
+  ) {
+    rid[i] <- dup[i,1]
+  } else if(
+    shp.hf.filtered$code6[dup[i,1]] != shp.hf.filtered$code6[dup[i,2]] &
+    shp.hf.filtered$code6[dup[i,1]] < shp.hf.filtered$code6[dup[i,2]]
+  ) {
+    rid[i] <- dup[i,2]
+  } else {
+    rid[i] <- dup[i,sample(1:2, 1)]
+  }
+}
+
+cat('\n\nParsed', length(rid), 'duplicates')
+
+shp.hf.filtered <-
+  shp.hf.filtered %>%
+  slice(-rid) %>%
+  select(`heat-flow (mW/m2)`, geometry) %>%
+  rename(hf = `heat-flow (mW/m2)`)
+
+cat('\n\nFinal ThermoGlobe dataset contains', nrow(shp.hf.filtered), 'observations')
+cat('\n', rep('~', 60), sep='')
+
+# Read Lucazeau (2019) interpolation
+cat('\n', rep('~', 60), sep='')
+cat('\nReading similarity interpolation from Lucazeau (2019) ...')
+
+interp.luca <-
+  read_delim(
+    'data/tglobe/HFgrid14.csv',
+	  delim = ';',
+	  col_types = c('ddddd')
+  ) %>%
+  rename(
+    lon = longiyude,
+    lat = latitude,
+    est = HF_pred,
+    sigma = sHF_pred,
+    obs = Hf_obs
+  )
+
+# Make simple feature object and project to robinson pacific centered
+shp.interp.luca <-
+  interp.luca %>% 
+  st_as_sf(coords = c(1,2), crs = proj4.wgs) %>% 
+  st_transform(proj4.RP)
+
+# Extract 0.5˚ x 0.5˚ grid from Lucazeau (2019)
+cat('\n\nExtracting similarity interpolation grid locations')
+
+shp.grid <- st_geometry(shp.interp.luca)
+
+cat('\nTotal similarity grid size:', length(shp.grid))
+cat('\n', rep('~', 60), sep='')
+
+# Defining interpolation domain
+cat('\n', rep('~', 60), sep='')
+cat('\nDefining interpolation domain ...\n')
+
+# Draw 1000 km buffer around segment boundaries
+buf.dist <- 1000000
+
+cat('\nDrawing', buf.dist/1000, 'km buffers around segments')
+
+shp.buffer <-
+  shp.segs %>%
+  map(~st_buffer(.x, buf.dist, endCapStyle = 'ROUND'))
+
+shp.box <-
+  shp.buffer %>%
+  map(~st_bbox(.x) %>% bbox_widen(crs = proj4.RP))
+
+# Crop ThermoGlobe data to bounding boxes
+cat('\nCropping ThermoGlobe data to bounding boxes')
+
+shp.hf.crop <-
+  suppressWarnings({
+#     shp.box %>%
+    shp.buffer %>%
+#     map(~shp.hf.filtered %>% st_crop(.x))
+    map(~shp.hf.filtered %>% st_intersection(.x) %>% select(-segment))
+  })
+
+cat('\nNumber of observations by segment:')
+cat('\n\n', rep('+', 40), sep='')
+walk2(shp.hf.crop, seg.names, ~{
+  cat('\n', ..2, ':', nrow(..1))
+})
+cat('\n', rep('+', 40), sep='')
+
+# Cropped grids from Lucazeau (2019) to bounding boxes
+cat('\n\nCropping interpolation grids to bounding boxes')
+
+shp.grid.crop <-
+#   shp.box %>%
+  shp.buffer %>%
+#   map(~shp.grid %>% st_crop(.x))
+  map(~shp.grid %>% st_intersection(.x))
+
+cat('\nNumber of Kriging estimates by segment:')
+cat('\n\n', rep('+', 40), sep='')
+walk2(shp.grid.crop, seg.names, ~{
+  cat('\n', ..2, ':', length(..1))
+})
+cat('\n', rep('+', 40), sep='')
+
+# Clean up environment
+cat('\n\n', rep('~', 60), sep='')
+cat('\nCleaning up environment ...')
+
+rm(list = lsf.str())
+
+rm(
+   files,
+   i,
+   shp.sliver,
+   dup,
+   rid,
+   buf.dist,
+   interp.luca,
+   package.list
+)
+
+# Save
+cat('\nSaving data to: data/hf.RData')
+save.image('data/hf.RData')
+
+cat('\nDone!')
+cat('\n', rep('~', 60), sep='')
