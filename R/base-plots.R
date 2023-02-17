e8c5cbc8e305bc33a616bb3b0c323e8b61e82e74
Buchanan Kerswell <buck.kerswell@gmail.com>
2023-02-17 09:33:53 -0500

    17 feb 2023 update; improve base plots

--- R/base-plots.R
+++ R/base-plots.R
@@ -16,25 +16,13 @@ cat('\nVisualizing ...')
 # Global ThermoGlobe buffer
 p1 <-
   ggplot() +
-  geom_sf(data = shp.world, size = 0.1, fill = 'grey70', color = 'black') +
-  geom_sf(data = st_union((bind_rows(shp.buffer))), size = 0.3, fill = 'black', alpha = 0.3) +
+  geom_sf(data = shp.world, size = 0.1, fill = 'grey80', color = 'grey60') +
+  geom_sf(data = st_union((bind_rows(shp.buffer))), size = 0.3, fill = 'black', alpha = 0.6) +
   geom_sf(data = shp.ridge, size = 0.4, color = 'black', alpha = 0.8) +
   geom_sf(data = shp.trench, size = 0.4, color = 'black', alpha = 0.8) +
   geom_sf(data = shp.transform, size = 0.4, color = 'black', alpha = 0.8) +
   geom_sf(data = bind_rows(shp.hf.crop), aes(color = hf), size = 0.3, shape = 20) +
   geom_sf(data = bind_rows(shp.segs), size = 0.8, color = 'white') +
-  annotate(
-    'label',
-    x = -Inf,
-    y = -Inf,
-    label = 'ThermoGlobe dataset',
-    size = 4,
-    hjust = 0,
-    vjust = 0,
-    fill = 'grey90',
-    label.padding = unit(0.15, 'lines'),
-    label.r = unit(0, 'lines')
-  ) +
   scale_color_viridis_c(
     option = 'magma',
     limits = c(0, 250),
@@ -43,30 +31,29 @@ p1 <-
   ) +
   scale_x_continuous(breaks = seq(-180, 180, 60)) +
   labs(color = bquote(mWm^-2)) +
-  coord_sf(expand = F) +
-  theme_map(font_size = 12) +
+  guides(color = guide_colorbar(title.position = 'top', title.hjust = 1)) +
+  coord_sf() +
+  theme_map(font_size = 14) +
   theme(
     axis.text = element_text(),
-    legend.position = c(1, 1),
-    legend.justification = c(1, 1),
+    legend.position = c(1, 0),
+    legend.justification = c(1, 0),
     legend.direction = 'horizontal',
-    legend.box.background = element_rect(fill = rgb(1, 1, 1, 0.8), color = NA),
-    legend.box.margin = margin(0, 8, 0, 0),
+    legend.box.background = element_blank(),
+    legend.box.margin = margin(1, 10, 1, 1),
     legend.margin = margin(),
     legend.key.height = unit(0.125, 'in'),
-    legend.key.width = unit(0.15, 'in'),
-    legend.title = element_text(vjust = 1, color = 'black', size = 10),
-    panel.grid = element_line(size = 0.1, color = 'white'),
-    panel.background = element_rect(fill = 'grey50', color = NA),
+    legend.key.width = unit(0.2, 'in'),
+    legend.title = element_text(vjust = 0, color = 'black', size = 14),
+    panel.grid = element_line(size = 0.01, color = 'grey60'),
     plot.margin = margin()
   )
-
 # Crop countries and similarity to buffer
 shp.world.buf <- suppressWarnings(shp.world %>% st_intersection(bind_rows(shp.buffer)))
 shp.sim <- suppressWarnings(shp.interp.luca %>% st_intersection(bind_rows(shp.buffer)))
 p2 <-
   ggplot() +
-  geom_sf(data = shp.world, size = 0.1, fill = 'grey70', color = 'black') +
+  geom_sf(data = shp.world, size = 0.1, fill = 'grey80', color = 'grey60') +
   geom_sf(data = shp.sim, aes(color = est.sim), size = 0.1, shape = 15) +
   geom_sf(data = shp.world.buf, size = 0.1, fill = 'grey70', alpha = 0.1) +
   geom_sf(data = shp.ridge, size = 0.4, color = 'black', alpha = 0.8) +
@@ -89,74 +76,54 @@ p2 <-
     force = 6,
     seed = 19
   ) +
-  annotate(
-    'label',
-    x = -Inf,
-    y = -Inf,
-    label = 'Similarity interpolation (Lucazeau, 2019)',
-    hjust = 0,
-    vjust = 0,
-    size = 4,
-    fill = 'grey90',
-    label.padding = unit(0.15, 'lines'),
-    label.r = unit(0, 'lines')
-  ) +
   labs(color = bquote(mWm^-2)) +
+  guides(color = guide_colorbar(title.position = 'top', title.hjust = 1)) +
   scale_color_viridis_c(
     option = 'magma',
     limits = c(0, 250),
     breaks = c(0, 125, 250),
     na.value = 'transparent'
   ) +
-  coord_sf(expand = F) +
-  theme_map(font_size = 12) +
+  coord_sf() +
+  theme_map(font_size = 14) +
   theme(
     axis.text = element_text(),
-    legend.position = c(1, 1),
-    legend.justification = c(1, 1),
+    legend.position = c(1, 0),
+    legend.justification = c(1, 0),
     legend.direction = 'horizontal',
-    legend.box.background = element_rect(fill = rgb(1, 1, 1, 0.8), color = NA),
-    legend.box.margin = margin(0, 8, 0, 0),
+    legend.box.background = element_blank(),
+    legend.box.margin = margin(1, 10, 1, 1),
     legend.margin = margin(),
     legend.key.height = unit(0.125, 'in'),
-    legend.key.width = unit(0.15, 'in'),
-    legend.title = element_text(vjust = 1, color = 'black', size = 10),
-    panel.grid = element_line(size = 0.1, color = 'white'),
-    panel.background = element_rect(fill = 'grey50', color = NA),
+    legend.key.width = unit(0.2, 'in'),
+    legend.title = element_text(vjust = 0, color = 'black', size = 14),
+    panel.grid = element_line(size = 0.01, color = 'grey60'),
     plot.margin = margin(1, 1, 1, 1)
   )
-
 # Global buffer composition
 p3 <-
   (p1 +
     annotate(
-      'label',
+      'text',
       label = 'a',
       x = -Inf,
       y = Inf,
-      size = 5,
+      size = 7,
       hjust = 0,
-      vjust = 1,
-      fill = 'grey90',
-      label.padding = unit(0.15, 'lines'),
-      label.r = unit(0, 'in')
+      vjust = 1
     ) +
     theme(axis.text = element_blank())
   ) /
   (p2 +
     annotate(
-      'label',
+      'text',
       label = 'b',
       x = -Inf,
       y = Inf,
-      size = 5,
+      size = 7,
       hjust = 0,
-      vjust = 1,
-      fill = 'grey90',
-      label.padding = unit(0.15, 'lines'),
-      label.r = unit(0, 'in')
-    ) +
-    theme(legend.position = 'none')
+      vjust = 1
+    )
   ) &
   theme(plot.margin = margin(1, 1, 1, 1))
 # Save
@@ -189,28 +156,9 @@ seg.names %>% walk(~{
   trench <- shp.trench.crop[[.x]]
   transform <- shp.transform.crop[[.x]]
   fts <- shp.fts[shp.fts$segment == .x,]
-  grats.x <-
-    as_tibble(st_coordinates(shp.grid.crop[[.x]])) %>%
-    group_by(Y) %>%
-    group_split() %>%
-    bind_rows(.id = 'line') %>%
-    mutate(line = as.integer(line))
-  grats.spacing <-
-    mean(
-      map_dbl(
-        seq_len(max(grats.x$line)-1),
-        ~{abs(
-          grats.x$Y[grats.x$line == .x][1] -
-          grats.x$Y[grats.x$line == .x+1][1]
-        )}
-      )
-    )
-  grats <-
-    st_make_grid(shp.grid.crop[[.x]], cellsize = c(grats.spacing, grats.spacing)) %>%
-    st_intersection(buf)
   pnt.size <- 3
-  annt.txt.size <- 4
-  base.txt.size <- 12
+  annt.txt.size <- 7
+  base.txt.size <- 14
   # Define map scale 1:50,000 [meters]
   wdth <- (st_bbox(buf)$xmax - st_bbox(buf)$xmin)/5e4
   hght <- (st_bbox(buf)$ymax - st_bbox(buf)$ymin)/5e4
@@ -235,34 +183,28 @@ seg.names %>% walk(~{
   # Interpolation domain
   pp1 <- 
     ggplot() +
-    geom_sf(data = world, size = 0.1, fill = 'grey70', color = 'black') +
-    geom_sf(data = grats, size = 0.1, color = 'black', fill = NA, alpha = 0.5) +
-    geom_sf(data = ridge, size = 1, color = 'black', alpha = 0.8) +
-    geom_sf(data = trench, size = 1, color = 'black', alpha = 0.8) +
-    geom_sf(data = transform, size = 1, color = 'black', alpha = 0.8) +
-    geom_sf(data = buf, size = 0.3, fill = NA) +
-    geom_sf(data = seg, size = 1.5, color = 'white') +
+    geom_sf(data = world, size = 0.1, fill = 'grey80', color = 'grey60') +
+    geom_sf(data = ridge, size = 1.5, color = 'black', alpha = 0.8) +
+    geom_sf(data = trench, size = 1.5, color = 'black', alpha = 0.8) +
+    geom_sf(data = transform, size = 1.5, color = 'black', alpha = 0.8) +
+    geom_sf(data = seg, size = 2, color = 'black') +
     geom_sf(data = hf, aes(color = hf), shape = 15, size = pnt.size*0.3) +
-    geom_sf(data = volc, size = pnt.size*0.3, color = 'gold', shape = 18) +
     geom_sf_label(
       data = fts,
       aes(label = label),
-      size = annt.txt.size,
+      size = annt.txt.size*0.5,
       fill = rgb(1, 1, 1, 0.8),
       label.padding = unit(0.15, 'lines'),
       label.r = unit(0.05, 'lines')
     ) +
     annotate(
-      'label',
+      'text',
       label = 'a',
       x = -Inf,
       y = Inf,
-      size = 5,
+      size = annt.txt.size,
       hjust = 0,
-      vjust = 1,
-      fill = 'grey90',
-      label.padding = unit(0.15, 'lines'),
-      label.r = unit(0, 'in')
+      vjust = 1
     ) +
     scale_color_viridis_c(
       option = 'magma',
@@ -271,21 +213,21 @@ seg.names %>% walk(~{
       na.value = 'transparent'
     ) +
     labs(color = bquote(mWm^-2)) +
-    coord_sf(expand = F) +
+    guides(color = guide_colorbar(title.position = 'top', title.hjust = 1)) +
+    coord_sf() +
     theme_map(font_size = base.txt.size) +
     theme(
       axis.text.x = element_text(angle = 30, hjust = 1, vjust = 1),
       axis.text.y = element_text(angle = 30, hjust = 1),
-      legend.position = c(1, 1),
-      legend.justification = c(1, 1),
+      legend.position = c(1, 0),
+      legend.justification = c(1, 0),
       legend.direction = 'horizontal',
-      legend.box.background = element_rect(fill = rgb(1, 1, 1, 0.8), color = NA),
-      legend.box.margin = margin(1, 8, 1, 2),
+      legend.box.background = element_blank(),
+      legend.box.margin = margin(1, 10, 1, 1),
       legend.key.height = unit(0.125, 'in'),
-      legend.key.width = unit(0.15, 'in'),
-      legend.title = element_text(vjust = 1),
-      panel.grid = element_line(size = 0.1, color = 'white'),
-      panel.background = element_rect(fill = 'grey50', color = NA),
+      legend.key.width = unit(0.2, 'in'),
+      legend.title = element_text(vjust = 0, color = 'black', size = base.txt.size),
+      panel.grid = element_blank(),
       plot.margin = margin(1, 1, 1, 1)
     )
   if(
@@ -298,25 +240,22 @@ seg.names %>% walk(~{
   # Similarity interpolation
   pp2 <- 
     ggplot() +
-    geom_sf(data = world, size = 0.1, fill = 'grey70', color = 'black') +
+    geom_sf(data = world, size = 0.1, fill = 'grey80', color = 'grey60') +
     geom_sf(data = sim, aes(color = est.sim), size = pnt.size, shape = 15, show.legend = F) +
-    geom_sf(data = world.buf, size = 0.1, color = 'grey80', fill = 'grey70', alpha = 0.1) +
-    geom_sf(data = ridge, size = 1, color = 'black', alpha = 0.8) +
-    geom_sf(data = trench, size = 1, color = 'black', alpha = 0.8) +
-    geom_sf(data = transform, size = 1, color = 'black', alpha = 0.8) +
-    geom_sf(data = buf, size = 0.3, fill = NA) +
-    geom_sf(data = seg, size = 1.5, color = 'white') +
+    geom_sf(data = world.buf, size = 0.1, fill = NA, color = 'grey60') +
+    geom_sf(data = ridge, size = 1.5, color = 'black', alpha = 0.8) +
+    geom_sf(data = trench, size = 1.5, color = 'black', alpha = 0.8) +
+    geom_sf(data = transform, size = 1.5, color = 'black', alpha = 0.8) +
+    geom_sf(data = buf, size = 0.1, fill = NA, color = 'grey60', alpha = 0.1) +
+    geom_sf(data = seg, size = 2, color = 'white') +
     annotate(
-      'label',
+      'text',
       label = 'b',
       x = -Inf,
       y = Inf,
-      size = 5,
+      size = annt.txt.size,
       hjust = 0,
-      vjust = 1,
-      fill = 'grey90',
-      label.padding = unit(0.15, 'lines'),
-      label.r = unit(0, 'in')
+      vjust = 1
     ) +
     scale_color_viridis_c(
       option = 'magma',
@@ -325,7 +264,7 @@ seg.names %>% walk(~{
       na.value = 'transparent'
     ) +
     labs(color = bquote(mWm^-2)) +
-    coord_sf(expand = F) +
+    coord_sf() +
     theme_map(font_size = base.txt.size) +
     theme(
       axis.text.x = element_text(angle = 30, hjust = 1, vjust = 1),
@@ -337,9 +276,8 @@ seg.names %>% walk(~{
       legend.box.margin = margin(0, 8, 0, 0),
       legend.key.height = unit(0.125, 'in'),
       legend.key.width = unit(0.15, 'in'),
-      legend.title = element_text(vjust = 1),
-      panel.grid = element_line(size = 0.1, color = 'white'),
-      panel.background = element_rect(fill = 'grey50', color = NA),
+      legend.title = element_text(vjust = 0, color = 'black', size = base.txt.size),
+      panel.grid = element_blank(),
       plot.margin = margin(1, 1, 1, 1)
     )
   if(
@@ -366,11 +304,10 @@ seg.names %>% walk(~{
       ) &
       theme(
         plot.margin = margin(1, 1, 1, 1),
-        panel.grid = element_line(size = 0.1, color = 'white'),
-        panel.background = element_rect(fill = 'grey50', color = NA),
+        panel.grid = element_blank(),
         legend.box.margin = margin(),
         legend.text = element_text(margin = margin(t = -4)),
-        legend.margin = margin(0, 8, 0, 0)
+        legend.margin = margin(1, 10, 1, 1)
       )
     # Save
     cat('\nSaving plot to: figs/base/', str_replace_all(.x, ' ', ''), 'Comp.png', sep = '')
@@ -402,11 +339,10 @@ seg.names %>% walk(~{
       ) &
       theme(
         plot.margin = margin(1, 1, 1, 1),
-        panel.grid = element_line(size = 0.1, color = 'white'),
-        panel.background = element_rect(fill = 'grey50', color = NA),
+        panel.grid = element_blank(),
         legend.box.margin = margin(),
         legend.text = element_text(margin = margin(t = -4)),
-        legend.margin = margin(0, 8, 0, 0)
+        legend.margin = margin(1, 10, 1, 1)
       )
     # Save
     cat('\nSaving plot to: figs/base/', str_replace_all(.x, ' ', ''), 'Comp.png', sep = '')
