6136cc14162f4fb753c06d19ad51ad20f7272f8c
Buchanan Kerswell <buck.kerswell@gmail.com>
2024-04-22 17:05:47 +0200

    22 apr 2024 updates; base map and opt kriging figure improvements

--- R/nloptr.R
+++ R/nloptr.R
@@ -9,10 +9,12 @@ load('assets/map_data/map-data.RData')
 v_mods <- c('Sph', 'Exp', 'Lin', 'Bes')
 zones <- c('NPA', 'SAM', 'SEA', 'SWP')
 
-# Optimize kriging models after Li et al. (2018)
-nlopt_transects(shp_submap$short_name, v_mods)
-
-# Visualize best variogram solutions
-walk(shp_submap$short_name, plot_optimal_variogram)
+walk(zones, ~{
+  # Optimize kriging models after Li et al. (2018)
+  ids <- shp_submap$short_name[shp_submap$zone == .x]
+  nlopt_transects(ids, v_mods)
+  # Visualize best variogram solutions
+  walk(ids, plot_optimal_variogram)
+})
 
 cat('\nnloptr.R complete!\n\n')
