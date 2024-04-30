20847aeca27541748a90d9a39303eb8e491989b4
Buchanan Kerswell <buck.kerswell@gmail.com>
2024-04-30 23:06:16 +0200

    30 apr 2024 update; remove Bes model and lower k.cv NA threshold

--- R/nloptr.R
+++ R/nloptr.R
@@ -5,28 +5,28 @@ cat(rep('~', 45), '\n', sep='')
 source('R/functions.R')
 load('assets/map_data/map-data.RData')
 
-# Optimize krige models and visualize results
+# Nlopt setup
+ids <- shp_submap$short_name
+alg <- 'NLOPT_LN_NELDERMEAD'
+v_mods <- c('Sph', 'Exp', 'Lin')
 fpath <- 'assets/nlopt_data/interpolation-summary.RData'
-if (!file.exists(fpath)) {
-  v_mods <- c('Sph', 'Exp', 'Lin', 'Bes')
-  alg <- 'NLOPT_LN_NELDERMEAD'
-  ids <- shp_submap$short_name
 
-  cat('\nOptimizing krige models after Li et al. (2018) ...\n')
+if (!file.exists(fpath)) {
+  # Optimize krige models and visualize results
   nlopt_transects(ids, v_mods, alg)
+
+  # Summarize nlopt results
+  nlopt_summary <- summarize_optimal_krige_models(ids)
+  interp_diff_summary <- summarize_interpolation_differences(ids)
+  interp_accuracy_summary <- summarize_interpolation_accuracy(ids)
+
+  # Save nlopt results
+  save(list=c('nlopt_summary', 'interp_diff_summary', 'interp_accuracy_summary'), file=fpath)
+
+  # Visualize nlopt results
   walk(ids, plot_transect)
   walk(ids, plot_optimal_krige_model)
 
-  cat('\nSummarizing optimal krige models ...\n')
-  nlopt_summary <- summarize_optimal_krige_models(ids)
-
-  cat('\nSummarizing interpolation differences ...\n')
-  interp_diff_summary <- summarize_interpolation_differences(ids)
-
-  cat('\nSummarizing interpolation accuracies ...\n')
-  interp_accuracy_summary <- summarize_interpolation_accuracy(ids)
-
-  save(list=c('nlopt_summary', 'interp_diff_summary', 'interp_accuracy_summary'), file=fpath)
 } else {
   cat('\nOptimal krige model summary found at:\n', fpath, '\n', sep='')
 }
