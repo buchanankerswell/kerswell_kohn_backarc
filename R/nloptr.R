922211be6c401aaa6a0c7c851675ac68784d440b
Buchanan Kerswell <buck.kerswell@gmail.com>
2024-06-24 21:30:38 +0200

    24 jun 2024 update; define krige.cv folds to fix spurious cost function; fig improvements

--- R/nloptr.R
+++ R/nloptr.R
@@ -4,34 +4,37 @@
 cat(rep('~', 45), '\n', sep='')
 source('R/functions.R')
 load('assets/map_data/map-data.RData')
+plan(multicore, workers=availableCores() - 2)
 
 # Nlopt setup
+seed <- 42
+alg <- 'NLOPT_LN_SBPLX'
 ids <- shp_submap$short_name
-alg <- 'NLOPT_LN_NELDERMEAD'
 v_mods <- c('Sph', 'Exp', 'Lin')
 fpath <- 'assets/nlopt_data/interpolation-summary.RData'
 
+# Optimize krige models
 if (!file.exists(fpath)) {
-  # Optimize krige models and visualize results
   nlopt_transects(ids, v_mods, alg)
-
-  # Summarize nlopt results
   nlopt_summary <- summarize_optimal_krige_models(ids)
   interp_diff_summary <- summarize_interpolation_differences(ids)
   interp_accuracy_summary <- summarize_interpolation_accuracy(ids)
-
-  # Save nlopt results
   save(list=c('nlopt_summary', 'interp_diff_summary', 'interp_accuracy_summary'), file=fpath)
-
-  # Visualize nlopt results
   plot_nlopt_summary()
   plot_control_point_summary()
-  walk(ids, plot_transect)
-  walk(ids, plot_optimal_krige_model)
-  walk(c('NPA', 'SAM', 'SEA', 'SWP'), plot_interp_accuracy_summary)
-
+  future_walk(ids, plot_transect_buff_comp, .options=furrr_options(seed=seed), .progress=T)
+  future_walk(ids, plot_optimal_krige_model, .options=furrr_options(seed=seed), .progress=T)
+  future_walk(c('NPA', 'SAM', 'SEA', 'SWP'), plot_interp_accuracy_summary,
+              .options=furrr_options(seed=seed), .progress=T)
 } else {
-  cat('\nOptimal krige model summary found at:\n', fpath, '\n', sep='')
+  cat('\nOptimal krige model summary found at:\n  ', fpath, '\n', sep='')
+  load('assets/nlopt_data/interpolation-summary.RData')
+  plot_nlopt_summary()
+  plot_control_point_summary()
+  future_walk(ids, plot_transect_buff_comp, .options=furrr_options(seed=seed), .progress=T)
+  future_walk(ids, plot_optimal_krige_model, .options=furrr_options(seed=seed), .progress=T)
+  future_walk(c('NPA', 'SAM', 'SEA', 'SWP'), plot_interp_accuracy_summary,
+              .options=furrr_options(seed=seed), .progress=T)
 }
 
 cat('\nnloptr.R complete!\n')
