074fe5d3d51b681fcf9a3871b10ec9949b32c41e
Buchanan Kerswell <buck.kerswell@gmail.com>
2024-06-26 10:49:48 +0200

    26 jun 2024 update; fig improvements; cleanup Makefile; refactor R scripts with main(); better printing; remove unused scripts and functions; add loess smooth models to projected obs; add strip plot;

--- R/check-packages.R
+++ R/check-packages.R
@@ -1,13 +1,11 @@
 #!/usr/bin/env Rscript
 
-# Quiet loading
 sshhh <- function(p) {
   suppressWarnings(suppressPackageStartupMessages({
-    require(p, quietly = T, character.only = TRUE)
+    require(p, quietly=T, character.only=TRUE)
   }))
 }
 
-# Install dependencies
 check_dependencies <- function(packages) {
   pkgs <- unlist(list(packages))
   req <- unlist(lapply(pkgs, sshhh))
@@ -19,30 +17,29 @@ check_dependencies <- function(packages) {
   req <- unlist(lapply(pkgs, sshhh))
   need <- pkgs[req == FALSE]
   if (length(need) > 0) {
-    cat('\n', rep('~', 45), sep = '')
+    cat('\n', rep('~', 45), sep='')
     cat('\nFailed to install packages:\n')
     writeLines(need)
     cat('\nFor troubleshooting tips see:')
     cat('\nhttps://github.com/buchanankerswell/kerswell_kohn_backarc')
-    cat('\n', rep('~', 45), '\n', sep = '')
+    cat('\n', rep('~', 45), '\n', sep='')
     stop()
   }
 }
 
-# Package list
-package_list <- c('tictoc', 'stringr', 'tidyr', 'readr', 'readxl', 'purrr', 'furrr',
-                  'tibble', 'dplyr', 'magrittr', 'units', 'ggplot2', 'colorspace', 'metR',
-                  'ggrepel', 'ggridges', 'ggnewscale', 'patchwork', 'cowplot', 'ggsflabel',
-                  'marmap', 'scales', 'ggspatial', 'gstat', 'rgeos', 'sp', 'sf',
-                  'rnaturalearth', 'nloptr', 'zoo', 'jsonlite')
+main <- function() {
+  package_list <- c('tictoc', 'stringr', 'tidyr', 'readr', 'readxl', 'purrr', 'furrr',
+                    'tibble', 'dplyr', 'magrittr', 'units', 'ggplot2', 'colorspace', 'metR',
+                    'ggrepel', 'ggridges', 'ggnewscale', 'patchwork', 'cowplot', 'ggsflabel',
+                    'marmap', 'scales', 'ggspatial', 'gstat', 'rgeos', 'sp', 'sf',
+                    'rnaturalearth', 'nloptr', 'zoo', 'jsonlite')
+  cat('Checking required packages ...')
+  check_dependencies(package_list)
+  cat('\n', rep('~', 45), '\n', sep='')
+  print(sessionInfo())
+  cat(rep('~', 45), sep='')
+  cat('\nAll required packages installed and available!')
+  cat('\n', rep('=', 45), sep='')
+}
 
-# Check dependencies and install missing packages
-cat('Checking required packages ...')
-check_dependencies(package_list)
-
-# Print session info
-cat('\n', rep('~', 45), '\n', sep='')
-sessionInfo()
-
-cat('\nAll required packages installed and available!')
-cat('\ncheck-packages.R complete!\n')
+main()
--- R/test.R
+++ R/test.R
@@ -1,17 +1,9 @@
 #!/usr/bin/env Rscript
 
-ids <- shp_submap$short_name
+source('R/functions.R')
+load_map_data('assets/map_data/map-data.RData')
+load_nlopt_data('assets/nlopt_data/interpolation-summary.RData')
 
 source('R/functions.R')
-load('assets/map_data/map-data.RData')
-load('assets/nlopt_data/interpolation-summary.RData')
-
-source('R/functions.R')
-file.remove('figs/transect_buff/NPA02-transect-buff-comp.png')
-plot_transect_buff_comp(ids[2])
-
-plan(multicore, workers=availableCores() - 2)
-future_walk(seq(length(ids) - 2), ~{
-  ids_triplet <- ids[.x:(.x + 2)]
-  plot_transect_neighbors_comp(ids_triplet)
-}, .options=furrr_options(seed=seed), .progress=T)
+ids <- shp_submap$short_name[c(1:11, 43:47)]
+plot_transect_strip_comp(ids)
