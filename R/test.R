31ed5b37a819e795e53c148cadd537d6d23d98a4
Buchanan Kerswell <buck.kerswell@gmail.com>
2024-06-27 00:09:20 +0200

    make loess fit same distance interval for all transect profiles; use loess for geom_rug; refactor plot_transect_neighbors; add cross-correlation and xcorr plot

--- R/test.R
+++ R/test.R
@@ -4,6 +4,4 @@ source('R/functions.R')
 load_map_data('assets/map_data/map-data.RData')
 load_nlopt_data('assets/nlopt_data/interpolation-summary.RData')
 
-source('R/functions.R')
-ids <- shp_submap$short_name[c(1:11, 43:47)]
-plot_transect_strip_comp(ids)
+ids <- shp_submap$short_name
